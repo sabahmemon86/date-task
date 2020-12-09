e8b8c659b85f424c680c8f6db1bb30a0046c2dd0
sabahmemon <75626729+sabahmemon86@users.noreply.github.com>
2020-12-09 19:44:42 +0500

    Add files via upload
    date task on 7 december

---
+++ date.js
@@ -0,0 +1,42 @@
+console.log('          DATE TASK             ');
+console.log(' -----------------------------   ');
+
+
+var its = new Date("2020 , 12 , 07")
+console.log(its);
+
+var its = new Date()
+console.log(its.getMonth());
+
+var its = new Date()
+console.log(its.getSeconds());
+
+var its = new Date()
+console.log(its.getHours());
+
+var its = new Date()
+console.log(its.getDay());
+
+var its = new Date()
+console.log(its.getMinutes());
+
+var its = new Date();
+its.setFullYear(2020);
+console.log(its);
+
+var its = new Date();
+its.setDate(16);
+console.log(its);
+
+var its = new Date();
+its.setMonth(09);
+console.log(its);
+
+var its = new Date();
+its.setHours(04);
+console.log(its);
+
+var its = new Date();
+its.setSeconds(52);
+console.log(its);
+
